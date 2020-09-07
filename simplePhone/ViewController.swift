879fedee8b2195599b58a322ce851c1083d29a2b
笹倉一也 <kazuya.sasakura@gmail.com>
2020-09-07 16:14:27 +0900

    Initial Commit

---
+++ simplePhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simplePhone
+//
+//  Created by 笹倉一也 on 2020/09/07.
+//  Copyright © 2020 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
