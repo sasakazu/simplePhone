50c0f4576f31cbc86c4d9d0d1c8212c0c380ece6
笹倉一也 <kazuya.sasakura@gmail.com>
2020-09-08 20:22:38 +0900

    ログインサインアップとファイルの追加

---
+++ simplePhone/login.swift
@@ -0,0 +1,22 @@
+//
+//  login.swift
+//  simplePhone
+//
+//  Created by 笹倉一也 on 2020/09/08.
+//  Copyright © 2020 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
---
+++ simplePhone/signup.swift
@@ -0,0 +1,22 @@
+//
+//  signup.swift
+//  simplePhone
+//
+//  Created by 笹倉一也 on 2020/09/08.
+//  Copyright © 2020 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class signup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
